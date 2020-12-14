32e4213806cbcc083b2157cc5051fb563e78e4de
AlejoRodriguez <juarodriguezt@correo.udistrital.edu.co>
2020-12-13 21:25:40 -0500

    Actualizacion rutas Backend

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from db.reserva_db import ReservaInDB
 from db.reserva_db import update_reserva, get_reserva, database_reservas
 from models.reserva_models import ReservaIn, ReservaOut
 from datetime import datetime, date
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
 
 api = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://frontend-proyhotel.herokuapp.com"
+    
+]
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
 consecutivo=5
 reserva_in_db:ReservaInDB
 
@@ -15,13 +30,14 @@ async def listar_reserva(num_reserva: str):
     reserva_in_db = get_reserva(num_reserva)
 
     if reserva_in_db == None:
-        raise HTTPException(status_code=404, detail="La reserva no existe")
+        return{"mensaje":"La reserva no existe"}
+        #raise HTTPException(status_code=404, detail="La reserva no existe")
 
     
     reserva_out = ReservaOut(**reserva_in_db.dict())
 
     return  reserva_out
-
+"""
 @api.post("/reserva/crear")
 async def crear_reserva(nombre:str, identificacion:str, fecha_inicio:str, fecha_fin:str, tipo_habitacion:str):
     global consecutivo
@@ -59,4 +75,53 @@ async def crear_reserva(nombre:str, identificacion:str, fecha_inicio:str, fecha_
     
     database_reservas[reserva_in_db.num_reserva]=reserva_in_db
 
-    return "Se ha creado la reserva "+idreserva
+    return "Se ha creado la reserva "+idreserva"""
+
+@api.post("/reserva/crear")
+async def crear_reserva(reserva:ReservaIn):
+    global consecutivo
+ 
+    #fecha_inicial=reserva.fecha_inicial.replace("-","/")
+    #fecha_final=reserva.fecha_inicial.replace("-","/")
+    
+    try:
+        fechai = datetime.strptime(reserva.fecha_inicial, '%Y-%m-%d')
+        fechaf = datetime.strptime(reserva.fecha_final, '%Y-%m-%d')
+        
+    except:
+        raise HTTPException(status_code=422, detail="Formato de fecha inválido. debe ser aaaa-mm-dd")
+    
+
+    if(fechaf <= fechai):
+        return{"mensaje":"La fecha final debe ser mayor que la fecha inicial"}
+        #raise HTTPException(status_code=422, detail="La fecha final debe ser mayor que la fecha inicial")
+
+    
+    fecha=datetime.now()
+    
+    if(fechai<fecha):
+         return{"mensaje":"La fecha inicial debe ser mayor que la fecha actual"}
+         #raise HTTPException(status_code=422, detail="La fecha inicial debe ser mayor que la fecha actual")
+   
+    dias= (fechaf-fechai).days
+    if(reserva.tipo_hab == "Sencilla"):
+        valor=100000*dias
+    elif(reserva.tipo_hab == "Doble"):
+        valor=150000*dias
+    else:
+        valor=200000*dias 
+    consecutivo+=1
+    pre ="rsrv_"
+    idreserva=pre+str(consecutivo)
+    reserva_in_db=ReservaInDB(**{"nombre":reserva.nombre,
+                            "identificacion":reserva.identificacion,
+                            "fecha":fecha.strftime('%Y-%m-%d %H:%M:%S'),
+                            "fecha_inicial":reserva.fecha_inicial,
+                            "fecha_final":reserva.fecha_final,
+                            "tipo_hab":reserva.tipo_hab,
+                            "valor":valor,
+                            "num_reserva": idreserva})
+           
+    database_reservas[reserva_in_db.num_reserva]=reserva_in_db
+
+    return {"mensaje":"Se ha creado la reserva "+idreserva}
